3780a0dad0d5e7233f68e6b4dcc513bfe15251b7
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2023-12-27 22:02:51 +0530

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,22 +1,18 @@
-javascript: (function () { // enclosing whole driver code as function to use it as bookmark
+javascript:(function () { 
   alert("Script Added");
-  var counter = 0,intervalId,checkCalendar,greenDay;
+  var counter = 0, intervalId, checkCalendar, greenDay;
+  
   function lC() {
     if (counter < 500) {
-      // Check each time wether dates were available or not after calling the script
-      // i.e. automatically terminating the loop if green day is found
       checkCalendar = document.getElementById("ui-datepicker-div");
       greenDay = checkCalendar.querySelectorAll(".greenday");
       if (greenDay.length > 0) {
         console.log("Dates Found, Script terminated...");
         clearTimeout(intervalId);
-      } else { // calling script from github hosted site
-        // This is unreliable method because it creates
-        // script tag each & every time. Try to find any
-        // alternative method of calling script.
+      } else { 
         var script = document.createElement("script");
         script.type = "text/javascript";
-        script.src =  "https://aditya4478.github.io/Visa-Scheduling-Script/index.js";
+        script.src = "https://aditya4478.github.io/Visa-Scheduling-Script/index.js";
         script.onload = function () {
           counter++;
           intervalId = setTimeout(lC, 6000);
@@ -27,19 +23,18 @@ javascript: (function () { // enclosing whole driver code as function to use it 
       alert("Refresh Page. Because script tags are appended too many times...");
     }
   }
-  // manually ending the loop by pressing "e"
+
   document.addEventListener("keydown", function (event) {
     if (event.key === "e") {
       clearTimeout(intervalId);
       console.log("User terminated script before dates found...");
     }
   });
-  // manually starting the loop by pressing "e"
+
   document.addEventListener("keydown", function (event) {
     if (event.key === "s") {
       lC();
       console.log("User started Script...");
     }
   });
-
 })();
