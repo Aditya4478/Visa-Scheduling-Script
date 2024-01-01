7844615dd76f0761a02aa721e200cf54fdf37dba
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2024-01-01 21:15:07 +0530

    Update cr.js

--- cr.js
+++ cr.js
@@ -40,15 +40,24 @@ function checkCity(m){
         return new Promise((resolve, reject) => {
             function checkDatepickerClass() {
             let datepicker = document.getElementById('datepicker');
+                if(eventListnerFlag == 1){
+                    eventListnerFlag = 0;
+                    //console.log("Event Key Listner added");
+                    document.addEventListener("keydown", function (event) {
+                        if (event.key === "e" || event.key === "E") {
+                            waitForDatepickerClass = null
+                            checkCity = null;
+                            alert("User terminated script before dates found...");
+                        }
+                    });
+                }
                 //let nullDates = document.getElementById('datepicker').value;
                 if(document.getElementById('datepicker').value == ''){
-                    if(cityIndex < 6){
+                    if(cityIndex < 5){
                         cityIndex++;
                         checkCity(cityIndex);
-                    }
-                    reject();
-                }
-                if (datepicker && datepicker.classList.contains('hasDatepicker')) {
+                    }else{checkCity(1);}
+                }else if (datepicker && datepicker.classList.contains('hasDatepicker')) {
                     // Datepicker has the specified class
                     //console.log('Datepicker has the "hasDatepicker" class');
                     resolve();
@@ -61,7 +70,6 @@ function checkCity(m){
             checkDatepickerClass();
         });
     }
-
 
     waitForDatepickerClass().then(() => {
         // Perform further actions or logic when the datepicker has the specified class
@@ -120,11 +128,6 @@ function checkCity(m){
                                 $('#datepicker').datepicker('hide');
 
                                 if (greenDays.length === 0) {
-                                    // Select the first option in the city_dropdown - that is reseting all values
-
-                                    // city_dropdown.selectedIndex = 0;
-                                    // city_dropdown.dispatchEvent(new Event("change"));
-
                                     datepicker = undefined;
                                     calendarContainer = undefined;
                                     nextButton = undefined;
